8c932b37c16f7555b28d68e2732cbc1be82efb6a
Seb Jagoe <seb@Sebs-MacBook-Pro.local>
2025-04-01 08:06:43 -0400

    nav links corrected

--- src/lib/data/navLinks.js
+++ src/lib/data/navLinks.js
@@ -3,6 +3,6 @@
 export const navLinks = [
 	{ name: 'Home', href: '/', layout: 'both', location: 'both' },
 	{ name: 'About', href: '/about', layout: 'both', location: 'both' },
-	{ name: 'Blog', href: '/blog', layout: 'both', location: 'both' },
+	{ name: 'Blog', href: '/about', layout: 'both', location: 'both' },
 	{ name: 'Contact', href: '/contact', layout: 'mobile', location: 'footer' }
 ];
