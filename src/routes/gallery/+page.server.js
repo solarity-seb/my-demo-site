f01f6f5ea38ca8df50a83584aecd5e511abcbf2e
Seb Jagoe <seb.jagoe@gmail.com>
2025-04-02 12:49:58 -0400

    gallery server ssr fix

--- src/routes/gallery/+page.server.js
+++ src/routes/gallery/+page.server.js
@@ -1,6 +1,9 @@
 // src/routes/gallery/+page.server.js
 import { sanity } from '$lib/sanityClient';
 
+export const ssr = false;
+
+
 export async function load() {
     const query = `*[_type == "galleryImage" && defined(image.asset)] | order(_createdAt desc){
         _id,
