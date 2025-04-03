2635ec7cbe8ed833e2c75452c4214a3afe1806b3
Seb Jagoe <seb.jagoe@gmail.com>
2025-04-03 18:31:36 -0400

    deleted home page

---
+++ src/lib/components/Buttons.svelte
@@ -0,0 +1,25 @@
+<!-- Buttons.svelte -->
+
+<script lang="ts">
+  export let primaryText: string = '';
+  export let primaryLink: string = '';
+  export let secondaryText: string = '';
+  export let secondaryLink: string = '';
+  export let lineText: string = '';
+  export let lineLink: string = '';
+  export let centered: boolean = true;
+</script>
+
+<div class={`btn-container u-flex u-flex-v-center${centered ? ' u-flex-h-center' : ''}`}>
+  {#if primaryText && primaryLink}
+    <a href={primaryLink} class="btn btn-primary">{primaryText}</a>
+  {/if}
+
+  {#if secondaryText && secondaryLink}
+    <a href={secondaryLink} class="btn btn-secondary">{secondaryText}</a>
+  {/if}
+
+  {#if lineText && lineLink}
+    <a href={lineLink} class="line-link">{lineText}</a>
+  {/if}
+</div>
